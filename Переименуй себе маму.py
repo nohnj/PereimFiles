17b683926c89b666be3592bd892d4f1c5f5f2d94
Hihahita <justrises1337@gmail.com>
2023-10-21 11:11:08 +0300

    Update Переименуй себе маму.py

--- Переименуй себе маму.py
+++ Переименуй себе маму.py
@@ -2,13 +2,6 @@ import os
 import shutil
 import random
 from pathlib import Path
-from PIL import Image
-
-new_dir_name = "Бубис"
-name_generator = 0
-debug_dir = Path("C:/Серьёзное дело/Переименовываем/" + str(new_dir_name))
-parent_dir = Path("C:/Серьёзное дело/Переименовываем")
-path = parent_dir / new_dir_name
 
 def is_directory_empty(directory):
     for file in os.listdir(directory):
@@ -16,23 +9,17 @@ def is_directory_empty(directory):
             return False
     return True
 
-if not parent_dir.exists():
-    raise FileNotFoundError(f"{parent_dir} не существует, идиот")
-if path.exists():
-    shutil.rmtree(str(path))
-try:
-    path.mkdir()
-    print(f"Папка {new_dir_name} создана в {parent_dir}")
-except FileExistsError:
-    if is_directory_empty(debug_dir) == True:
-        print(f"Папка {new_dir_name} из {debug_dir }пуста. Начинаю работу\n")
-    else:
-        while is_directory_empty == False:
-            new_dir_name = "Бубис" + str(name_generator)
-            path.mkdir()
-
 i = 0
 ext = ["jpg","webp","jpeg"]
+dir_name = "Бубис"
+name_generator = 0
+debug_dir = Path("C:/Серьёзное дело/Переименовываем/" + str(dir_name))
+parent_dir = Path("C:/Серьёзное дело/Переименовываем")
+path = parent_dir / dir_name
+
+if debug_dir.exists():
+    shutil.rmtree(debug_dir)
+    debug_dir.mkdir()
 
 try:
     for file in os.listdir(parent_dir):
@@ -53,27 +40,38 @@ try:
             print(fr'{file} moved form {parent_dir} to {debug_dir} as {i}.png')
             i += 1
 except OSError:
-    print('\nSomething went wrong')
+    print('\nSuccess')
 
+i = 0
 shufflee = 0
 shuffle_ls = []
 Shuffle = input('Would you like to shuffle files?(y/n) \n ')
-if Shuffle.lower == 'yes' or 'y':
-    for y in range(i):
-        shuffle_ls.append(y)
-        random.shuffle(shuffle_ls)
+
+if Shuffle.lower() == 'y':
     for file in os.listdir(debug_dir):
-        # try:
-        os.rename(fr'{parent_dir}/Бубис/{i}.{os.path.splitext(file)[1][1:]}', fr'{parent_dir}/{shuffle_ls[shufflee]}.{os.path.splitext(file)[1][1:]}')
-        print(fr'{file} moved form {parent_dir} to {debug_dir} as {i}.png')
-        # except OSError:
-        #     print('\nSomething went wrong\n')
+        shuffle_ls.append(os.path.splitext(file)[0])
+    random.shuffle(shuffle_ls)
+    for file in os.listdir(debug_dir):
+        try:
+            os.rename(fr'{debug_dir}/{file}', fr'{parent_dir}/{shuffle_ls[shufflee]}.{os.path.splitext(file)[1][1:]}')
+            print(f'{file} moved from {debug_dir} back to {parent_dir} as {shuffle_ls[shufflee]}.{os.path.splitext(file)[1][1:]}')
+            i += 1
+        except FileNotFoundError:
+            print('\nSomething went wrong\n')
         shufflee += 1
-    # shutil.rmtree(debug_dir)
+    shutil.rmtree(debug_dir)
     print("Thanks for using dis shit yo. Ima like puttd ma soul init")
-elif Shuffle.lower == 'no' or 'n':
-    print("GO FUCK YOURSELF")
-    breakpoint()
+
+elif Shuffle.lower() == 'n':
+    for file in os.listdir(debug_dir):
+        try:
+            fael = file
+            os.rename(fr'{debug_dir}/{fael}', fr'{parent_dir}/{fael}')
+            print(f'{fael} moved from {debug_dir} back to {parent_dir} as {fael}')
+        except FileNotFoundError:
+                print('\nI couldnt find the thing')
+    shutil.rmtree(debug_dir)
+    print("Its not perfectly the same BUT ITS ONLY YOUR FAULT\nGO FUCK YOURSELF")
+
 else:
     print("https://support.microsoft.com/ru-ru/windows/использование-клавиатуры-18b2efc1-9e32-ba5a-0896-676f9f3b994f")
-    breakpoint()
